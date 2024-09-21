d951722cb1058937c53eae1213f8f5b1adf70f1c
Mohamed Louazir <mlouazir@e3r11p4.1337.ma>
2024-09-21 08:50:08 +0100

    f

--- 03/ex00/ClapTrap.hpp
+++ 03/ex00/ClapTrap.hpp
@@ -11,28 +11,18 @@ private:
     int energyPoints;
     int attackDamage;
 public:
-    ClapTrap() : hitPoints(10), energyPoints(10), attackDamage(0) {std::cout << "Default Constructor Called\n";};
-    ClapTrap( std::string& name ) : name(name), hitPoints(10), energyPoints(10), attackDamage(0) {std::cout << name << " Constructor Called\n";};
-    ClapTrap( ClapTrap& obj ) {
-        *this = obj;
-    };
-    ClapTrap& operator=( ClapTrap& obj) {
-        if (this != &obj) {
-            this->name = obj.getName();
-            this->hitPoints = obj.getHitPoints();
-            this->energyPoints = obj.getEnergyPoints();
-            this->attackDamage = obj.getAttackDamage();
-        }
-        return *this;
-    };
+    ClapTrap();
+    ClapTrap( const std::string& name );
+    ClapTrap( const ClapTrap& obj );
+    ClapTrap& operator=( const ClapTrap& obj);
     void attack( const std::string& target );
     void takeDamage( unsigned int amount );
     void beRepaired( unsigned int amount );
-    std::string getName( void ) {return this->name;};
-    int getHitPoints( void ) {return this->hitPoints;};
-    int getEnergyPoints( void ) {return this->energyPoints;};
-    int getAttackDamage( void ) {return this->attackDamage;};
-    ~ClapTrap() {std::cout << name << " Destructor Called\n";};
+    std::string getName( void ) const;
+    int getHitPoints( void ) const;
+    int getEnergyPoints( void ) const;
+    int getAttackDamage( void ) const;
+    ~ClapTrap();
 };
 
 #endif
